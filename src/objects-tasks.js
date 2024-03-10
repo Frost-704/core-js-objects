cea2cc9d22f7b5f0b4846a55799438899bc7e3e2
Frost-704 <boolean.pg.me@gmail.com>
2024-03-10 21:07:19 +0300

    feat: finish Rectangle task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -182,8 +182,15 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const result = {
+    width,
+    height,
+    getArea() {
+      return width * height;
+    },
+  };
+  return result;
 }
 
 /**
